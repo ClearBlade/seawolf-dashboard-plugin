450f9c6b0d14c7bf139466e598bd3eb00f95f716
Sasa Pettyjohn <sasa@Sasas-MacBook-Pro.local>
2023-12-20 09:17:18 -0800

    hook up version 0.5 of react mfe

--- src/root.component.tsx
+++ src/root.component.tsx
@@ -1,5 +1,5 @@
 import { getBasePath } from '@clearblade/ia-mfe-core';
-import { appQueryClient } from '@clearblade/ia-mfe-react';
+import { appQueryClient, RecoilRoot } from '@clearblade/ia-mfe-react';
 import { SnackbarProvider } from 'notistack';
 import { QueryClientProvider } from 'react-query';
 import { BrowserRouter } from 'react-router-dom';
@@ -7,12 +7,14 @@ import DashboardPlugin from './components/DashboardPlugin';
 
 export default function Root() {
   return (
-    <BrowserRouter basename={getBasePath()}>
-      <QueryClientProvider contextSharing client={appQueryClient}>
-        <SnackbarProvider>
-          <DashboardPlugin />
-        </SnackbarProvider>
-      </QueryClientProvider>
-    </BrowserRouter>
+    <RecoilRoot>
+      <BrowserRouter basename={getBasePath()}>
+        <QueryClientProvider contextSharing client={appQueryClient}>
+          <SnackbarProvider>
+            <DashboardPlugin />
+          </SnackbarProvider>
+        </QueryClientProvider>
+      </BrowserRouter>
+    </RecoilRoot>
   );
 }
